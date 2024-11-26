37e07cc9d57c7c11ebcca70af145fda24f67e0c1
yasmi <yas1cf1@gmail.com>
2024-11-26 13:43:57 -0300

    buy component

--- src/components/buy.tsx
+++ src/components/buy.tsx
@@ -1,37 +1,47 @@
-"use client"
+"use client";
 import { ROUTES } from "@/app/constants/routes";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 
-export const BtBuy = ({idProduct}:{idProduct:string})=>{
+export const BuyButton = ({ productId }: { productId: string }) => {
   const router = useRouter();
-    const buy= async()=>{
-        if(!sessionStorage.getItem("Token")){
-          router.push(ROUTES.login)
-        }
-        try{
-            const response = await fetch('http://localhost:8080/cart/add', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'authorization': `${sessionStorage.getItem("Token")}`
-              },
-              body: JSON.stringify({
-                IdProduct:idProduct,
-                quantity:1
-              }),
-            });
-            if(response.status === 401){
-              router.push(ROUTES.login)
-            }
-            const result = await response.json();
-            console.log(result)
-          }catch(err){
-            console.log(err)
-          }
+
+  const handleBuy = async () => {
+    const token = sessionStorage.getItem("Token");
+
+    if (!token) {
+      return router.push(ROUTES.login);
     }
-    return(
-        <>
-          <button className="w-2/3 bg-black text-white rounded-[20px] p-2 m-2" onClick={()=>{buy()}}>Comprar</button>
-        </>
-    )
-}
+
+    try {
+      const response = await fetch("http://localhost:8080/cart/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: token,
+        },
+        body: JSON.stringify({
+          productId,
+          quantity: 1,
+        }),
+      });
+
+      if (response.status === 401) {
+        return router.push(ROUTES.login);
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <button
+      className="w-2/3 bg-black text-white rounded-[20px] p-2 m-2"
+      onClick={handleBuy}
+    >
+      Buy
+    </button>
+  );
+};
